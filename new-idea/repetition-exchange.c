1bab2756ac11bf13a2860ba76203a7b71fadbcab
smilecold <691823598@qq.com>
2016-07-16 23:17:58 -0700

    in train

--- new-idea/repetition-exchange.c
+++ new-idea/repetition-exchange.c
@@ -12,9 +12,12 @@ typedef struct relevance
   struct relevance * next;
 }rel;
 
-
+int fctral(int num);
+int cbntin(int rsp, int ssp);
+int arrang(int rsp, int ssp);
 int argnum(int num);
-void grpnum(int * grp, int num, int cnu, int cmn);
+int * grpnum(int num, int cnu, int cmn);
+int ** estarg(int argn, int num);
 
 int main(void)
 {
@@ -22,10 +25,10 @@ int main(void)
   int ** arg, **** exg, * grp;
   rel ** relh;
   argn = argnum(M);
-  grpnum(grp, M, N, G);
-  estarg(arg, argn, M);
-  estexg(exg, N, grp, G);
-  estrelh(relh, argn);
+  grp = grpnum(M, N, G);
+  arg = estarg(argn, M);
+  exg = estexg(N, grp, G);
+  relh = estrelh(argn);
   relargexg(arg, exg, M, N, argn, grp, G, relh);
   prin(arg, argn, relh, exg, G, grp, N, M, C);
   frerel(rel, argn);
@@ -35,22 +38,60 @@ int main(void)
   return 0;
 }
 
+int fctral(int num)
+{
+  int sum = 1, i;
+  for (i = 2; i <= num; i++)
+  {
+    sum *= i;
+  }
+  return sum;
+}
+
+int cbntin(int rsp, int ssp)
+{
+  int fc1, fc2, fc3, ctn;
+  fc1 = fctral(rsp);
+  fc2 = fctral(ssp);
+  fc3 = fctral(rsp - ssp);
+  ctn = fc1 / fc2 / fc3;
+  return ctn;
+}
+
+int arrang(int rsp, int ssp)
+{
+  int fc1, fc2, arg;
+  fc1 = fctral(rsp);
+  fc2 = fctral(ssp);
+  arg = fc1 / fc2;
+}
+
 int argnum(int num)
 {
-  int i, prd = 1;
-  for (i = 1; i <= num; i++)
-  {
-    prd *= i;
-  }
+  int prd;
+  prd = fctral(num);
   return prd;
 }
 
-void grpnum(int * grp, int num, int cnu, int cmn)
+int * grpnum(int num, int cnu, int cmn)
 {
-  int i;
-  grp = (int *)malloc(cnu * sizeof(int));
-  for (i = 1; i < cnu; i++)
+  int i, j, pow;
+  int * head, * pnt;
+  pnt = head = (int *)malloc(cnu * sizeof(int));
+  pow = cbntin(cnu, cmn);
+  for (i = 1; i <= cnu; i++)
   {
-    
+    *pnt = 1;
+    for(j = 0; j < i;j++)
+    {
+      (*pnt) *= pow;
+    }
+    pnt++;
   }
+  return head;
+}
+
+int ** estarg(int argn, int num)
+{
+  
 }
